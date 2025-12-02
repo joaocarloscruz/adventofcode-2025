8233b6b3e1ff8a90fede6381c70e53c38395fd60
joaocarloscruz <joaocruzbusiness@gmail.com>
2025-12-02 21:16:00 +0000

    add solution to part 2 of day 1

---
+++ day1/part2/Main.java
@@ -0,0 +1,53 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.List;
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        System.out.println(part2());
+    }
+
+    protected static List<String> readInput(String filePath) throws IOException {
+        List<String> lines = new ArrayList<>();
+        try (BufferedReader reader = new BufferedReader(new FileReader(filePath))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                lines.add(line);
+            }
+        }
+        return lines;
+    }
+
+    public static int part2() throws IOException {
+        List<String> lines = readInput("day1/part2/input.txt");
+        int password = 0;
+        int dial = 50;
+
+        for (String line : lines) {
+            char direction = line.charAt(0);
+            int amount = Integer.parseInt(line.substring(1));
+            int zeros = amount / 100;
+            int remainder = amount % 100;
+
+            password += zeros;
+
+            if (direction == 'L') {
+                if (dial != 0 && dial - remainder <= 0) { 
+                    password++;
+                }
+                dial = (dial - amount) % 100;
+                if (dial < 0) {
+                    dial += 100;
+                }
+            } else { 
+                if (dial + remainder >= 100) {
+                    password++;
+                }
+                dial = (dial + amount) % 100;
+            }
+        }   
+        return password;
+    }
+}
